e96f31cbd9fe63d1826ce45ff64501701918b17d
Silas Alves <silasalves.s@gmail.com>
2022-01-27 22:02:10 -0300

    creating tables and connecting to database

--- silasreceitas/settings.py
+++ silasreceitas/settings.py
@@ -65,8 +65,11 @@ WSGI_APPLICATION = 'silasreceitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'silas_receitas',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 

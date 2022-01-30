75b4079c0bded8c30ea8aea430e117653cd62fee
Silas Alves <silasalves.s@gmail.com>
2022-01-29 23:00:11 -0300

    improving the admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Receitas
 
-# Register your models here.
-admin.site.register(Receitas)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_de_preparo')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',) 
+    list_filter = ('categoria',)
+    list_per_page = 2
+
+admin.site.register(Receitas, ListandoReceitas)

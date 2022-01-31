ad86a71b1174a701b4d5ff251970d62ab45201be
Silas Alves <silasalves.s@gmail.com>
2022-01-31 15:02:06 -0300

    image upload fix

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,10 +1,10 @@
-from django.conf import Settings, settings
+from unicodedata import name
 from . import views
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:receita_id>', views.receita, name='receita')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('<int:receita_id>', views.receita, name='receita'),
+    path('buscar', views.buscar, name='buscar'),
+] 

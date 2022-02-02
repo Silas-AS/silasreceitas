9aacea4479447a34b3a846f47fb2e3ee0184d950
Silas Alves <silasalves.s@gmail.com>
2022-02-01 21:36:14 -0300

    created dashboard for users and added form for creating recipes

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
+from receitas.models import Receitas
 # Create your views here.
 def cadastro(request):
     if request.method == 'POST':
@@ -47,8 +48,35 @@ def login(request):
 
 
 def dashboard(request):
-    return render(request, 'usuarios/dashboard.html')
+    if request.user.is_authenticated:
+        id = request.user.id
+        receitas = Receitas.objects.order_by('-data_receita').filter(pessoa=id)
+
+        dados = { 
+            'receitas' : receitas
+        }
+        return render(request, 'usuarios/dashboard.html', dados)
+    else:
+        return redirect('index')
 
 
 def logout(request):
-    return render(request, 'usuarios/logout.html')
+    auth.logout(request)
+    return redirect('index')
+
+
+def cria_receita(request):
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receitas.objects.create(pessoa=user,nome_receita=nome_receita, ingredientes=ingredientes, modo_de_preparo=modo_preparo,tempo_de_preparo=tempo_preparo, rendimentos=rendimento,categoria=categoria, foto_receita=foto_receita)
+        receita.save()
+        return redirect('dashboard')
+    else:
+        return render(request, 'usuarios/cria_receita.html')
